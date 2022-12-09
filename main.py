ba1b6880a222c3ba33d8ea84363ef8ed4a023c0b
Padmanabhan <dev.pdmnhn@gmail.com>
2022-12-09 20:37:01 +0530

    Add CORS

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+)
 
 
 @app.get("/rfid")
